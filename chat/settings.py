78fe99aba211762a91e39fd911cdf84d6b354817
Satyam-droid <25satyam25@gmail.com>
2022-08-03 01:07:39 +0530

    final commit

--- chat/settings.py
+++ chat/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-1okgjcl4llwxrz+@b!x*3r$hmjh-h1ejmi@78o5@yeacx84#6x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -97,7 +97,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('REDIS_URL','redis://localhost:6379')],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 } 
